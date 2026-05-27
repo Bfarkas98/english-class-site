20121f2f6bb3237e707c28870bccc9ece77e5dab
Bfarkas98 <43004482+Bfarkas98@users.noreply.github.com>
2026-05-27 20:14:02 +0200

    fixing Clerk

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware(async (auth) => {
+export const proxy = clerkMiddleware(async (auth) => {
   await auth.protect()
 })
 
